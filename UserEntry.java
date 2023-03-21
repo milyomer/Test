5a3100629f0926a7b96c50ef2ee811bef9f2c54f
milyomer <127997088+milyomer@users.noreply.github.com>
2023-03-21 10:32:07 +0300

    Create UserEntry.java
    www.patika.dev

---
+++ UserEntry.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class UserEntry {
+	public static void main(String[] args) {
+		Scanner io = new Scanner(System.in);
+		
+		System.out.println("Lütfen kullanıcı adınızı giriniz: ");
+		String userName = io.next();
+		System.out.println("Lütfen şifrenizi giriniz: ");
+		String password = io.next();
+		String answer;
+		
+		if(
+				(userName.equals("patika") || userName.equals("Patika")) && password.equals("java123")
+				) {
+			System.out.println("Sisteme başarılı bir şekilde giriş yaptınız !");
+		} else if (password.equals("java123")) {
+					System.out.println("Kullanıcı adınız hatalıdır. Lütfen tekrar deneyiniz !");
+				}
+		 		else {
+			System.out.println("Şifreniz hatalıdır. Yeni şifre oluşturmak isterseniz lütfen \"evet\" olarak belirtin.");
+				answer = io.next();
+			System.out.println("Lütfen yeni şifrenizi giriniz: ");
+			password = io.next();
+				if (password.equals("java123")) {
+					System.out.println("Lütfen öncekinden farklı bir sifre girin");
+					password = io.next();
+					if (password.equals("java123")) {
+						System.out.println("Lütfen öncekinden farklı bir sifre girin");
+						
+					} else {
+						System.out.println("Yeni şifreniz oluşturuldu");
+					}
+				}
+		}
+	}
+}
